b90f503d0c90a875e3d6b9c1865337e36081cb22
Idire <idirexposito@gmail.com>
2018-10-22 12:55:57 +0200

    fdm update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,16 +1,11 @@
 var store = [{
-        "title": "MPCNC",
-        "excerpt":"Construimos una máquina CNC  ","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/mpcnc/",
-        "teaser":null},{
         "title": "Festival Impaciencia",
         "excerpt":"El 28 de abril de 2018 se estreno en la UMA el Festival Impaciencia, un evento lúdico y gratuito para poder ver, tocar y descubrir creaciones y dinámicas multidisciplinares relacionadas con la investigación, la comunicación, las ingenierías o el arte. Estaba muy centrado en la comunidad DIY y tenía planeadas...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/festival-impaciencia/",
+        "url": "/festival-impaciencia/",
         "teaser":null},{
         "title": "Constitucion",
         "excerpt":"Hoy ha sido un día decisivo en para todos los que participamos en esta iniciativa, y es que por fin nos hemos constituido como una asociación. El trámite no ha sido rápido ni fácil, hemos tenido que aprender sobre la marcha qué hacer y como hacerlo. Pero lo importante es...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/constitucion/",
+        "url": "/constitucion/",
         "teaser":null}]
--- formacion/fdm-basico.md
+++ formacion/fdm-basico.md
@@ -9,6 +9,6 @@ En este taller te enseñaremos los fundamentos de la impresión 3D, qué es posi
 
 La próxima edición se realizará en la Escuela de Ingenierías Industriales el miércoles 24 de octubre a las 17:00, aula 0.24.
 
-Lamentablemente ya no quedan plazas libres, pero puedes informarte de las siguientes convocatorias rellenando el siguiente [formulario](https://goo.gl/forms/AzABlRylEvvBmdYD3)
+Lamentablemente ya no quedan plazas libres, pero puedes mantenerte informado de las siguientes convocatorias rellenando el siguiente [formulario](https://goo.gl/forms/AzABlRylEvvBmdYD3).
 
 [Contenido del taller](/formacion/docs/fdm-basico.pdf)
